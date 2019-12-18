f59aa94c24d1039a63845db365446188389179bc
Eitan Sternlicht <eitanoffun@gmail.com>
2019-12-18 15:15:53 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div> 
+      <input
+        className="App"
+        type="text"
+        placeholder="choose repository path"
+        style={{ width: 200 }}
+      ></input>
+
+
+    </div>
+  );
+}
+
+export default App;
