89ea19a826e10b928abf235ea5806b871c8452af
Eitan Sternlicht <eitanoffun@gmail.com>
2020-01-14 16:32:05 +0200

    implement basic react client that calls our backend rest API

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div> 
-      <input
-        className="App"
-        type="text"
-        placeholder="choose repository path"
-        style={{ width: 200 }}
-      ></input>
-
-
-    </div>
-  );
+    const [descriptions, setDescription] = useState("");
+    const [results, setResults] = useState([]);
+    return (
+        <div>
+            <input
+                className="App"
+                type="text"
+                placeholder="choose repository path"
+                style={{ width: 200 }}
+                value={descriptions}
+                onChange={e => setDescription(e.target.value)}
+            ></input>
+            <button
+                onClick={() => {
+                    console.log("sending", descriptions);
+                    axios
+                        .post("http://localhost:3000/search", { data: { descriptions } })
+                        .then(response => {
+                            const { hits } = response.data.descriptions.body.hits;
+                            const sources = hits.map(({ _source }) => _source.descriptions);
+                            console.log("hits", sources);
+                            setResults(sources);
+                        })
+                        .catch(error => {
+                            console.log("error", error);
+                        });
+                }}
+            >
+                Send
+            </button>
+        {results.map(result => (<div key={result}>{result}</div>))}
+        </div>
+    );
 }
 
 export default App;
