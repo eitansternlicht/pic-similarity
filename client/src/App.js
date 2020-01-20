c208d416f31148facf4af4b5739eef214415d0c6
Roi Sulimani <roisulimani@gmail.com>
2020-01-20 11:13:21 +0200

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
+import ImageUploader from 'react-images-upload';
 
 function App() {
     const [descriptions, setDescription] = useState("");
     const [results, setResults] = useState([]);
+    const [picture, setPicture] = useState([]);
+
+    const onDrop = (pic) => {
+        setPicture(pic);
+        console.log(pic);
+        const formData = new FormData();
+        formData.append('file', pic);
+        formData.append('name', 'file')
+        axios({
+            method: 'post',
+            url: "http://localhost:3000/upload",
+            data: formData,
+            headers: {'Content-Type': 'multipart/form-data'}
+        })
+            // .post("http://localhost:3000/upload", { file: pic[0] })
+            .then(response => {
+                console.log("response", response);
+            })
+            .catch(error => {
+                console.log("error", error);
+            });
+    }
+
+    const onUpload = event => {
+        event.stopPropagation();
+
+    }
+
     return (
         <div>
             <input
@@ -34,6 +63,23 @@ function App() {
                 Send
             </button>
         {results.map(result => (<div key={result}>{result}</div>))}
+
+        {/* <ImageUploader
+            singleImage={true}
+            withIcon={true}
+            buttonText='Choose image'
+            onChange={onDrop}
+            imgExtension={['.jpg', '.gif', '.png', '.gif']}
+        /> */}
+        {/* <form><input type="file" /><button type="submit">Upload</button></form> */}
+
+        <h1>Upload Image</h1>
+ 
+         <form onSubmit={onUpload} action="http://localhost:3000/upload" method="post" enctype="multipart/form-data">
+            <input type="file" accept="image/*" name="file" />
+            <input type="submit" value="upload"/>
+        </form>
+
         </div>
     );
 }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { Client } = require('@elastic/elasticsearch');
+const multer = require('multer');
+
+const upload = multer({dest: __dirname + '/uploads'});
 const { imageClassification, imagesClassification, toESjson } = require('./classify');
 const client = new Client({
     node: 'http://localhost:9200/'
@@ -13,13 +16,33 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// app.use(express.static('public'));
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    console.log("fule", req.file);
+    if(req.file) {
+        res.json(req.file);
+    }
+    else throw 'error';
+});
+
+
 
 
 app.post('/search', (req, res) => {
     
-    toESjson("results.json");
+    // toESjson("results.json");
     // imagesClassification('/Users/roisulimani/Desktop/corpus');
     // console.log('req descriptions', req.body.data.descriptions);
+    const { pic } = req.body.data;
+
+    if(pic) {
+        console.log(pic)
+    }
+    else {
+        console.log("kchbvscjhbsv")
+
+        return;
     const { descriptions } = req.body.data;
 
     client
@@ -39,6 +62,7 @@ app.post('/search', (req, res) => {
             // console.log('error', error);
             res.send({ error: error });
         });
+    }
 });
 
 app.listen(port, () => {
