8043ea814ae074015dc475588c660f4e9cc59191
Roi Sulimani <roisulimani@gmail.com>
2020-06-03 11:41:11 +0300

    Data modification for doc2vec algorithm

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,11 @@
-import { DataDoc, addTfIdfVector, readJSON, writeJSON } from './utils/migrator';
+import {
+    DataDoc,
+    addTfIdfVector,
+    readJSON,
+    writeJSON,
+    toTerms,
+    addDocVectors,
+} from './utils/migrator';
 import { createWriteStream, readFileSync, writeFileSync } from 'fs';
 import express, { static as expressStatic } from 'express';
 import { json, urlencoded } from 'body-parser';
@@ -10,9 +17,12 @@ import { join } from 'path';
 import { makeUrl } from './utils/image-storage';
 import multer from 'multer';
 
-
-
-// const dataDocs: DataDoc[] = readJSON("../data.json");
+// const docsVectors = JSON.parse(
+//     readFileSync('../data_proccessing/doc2vec/docVectors.json').toString()
+// );
+// console.log(docsVectors.map(vec => vec.length));
+// const dataDocs: DataDoc[] = readJSON('../data.json');
+// addDocVectors(dataDocs, docsVectors);
 // const idToTerm = addTfIdfVector(dataDocs);
 // writeJSON(dataDocs, 'new-data.json');
 
@@ -30,6 +40,21 @@ import multer from 'multer';
 // console.log('newDataDocs', dataDocs[0]);
 // console.log('idToTerm[4]', idToTerm[4]);
 // console.log('idfMap[leisur]', idfMap['leisure']);
+
+// const dataDocs: DataDoc[] = readJSON('../data.json');
+// const docsTokens: string[][] = dataDocs.map(dataDoc => {
+//     return dataDoc._source.labelAnnotations.map(annotation => {
+//         return annotation.description;
+//     });
+// });
+// const doc2vecInputFile: string = docsTokens
+//     .map(docTokens => docTokens.flatMap(toTerms).join(' '))
+//     .join('\n');
+
+// writeFileSync('doc2vecInputFile.txt', doc2vecInputFile);
+
+// console.log(docsVectors);
+
 const upload = multer({ dest: __dirname + '/uploads' });
 const client = new Client({
     node: 'http://localhost:9200/',
@@ -37,7 +62,7 @@ const client = new Client({
 
 const app = express();
 const port = 3000;
-const constPath = 'C:\\Users\\user\\Desktop\\images\\unsplash\\'
+const constPath = 'C:\\Users\\user\\Desktop\\images\\unsplash\\';
 
 app.use(cors());
 app.use(urlencoded({ extended: false }));
@@ -48,56 +73,62 @@ app.use(expressStatic('static'));
 app.post('/upload', upload.single('file'), (req, res) => {
     if ('file' in req) {
         // const imagePath = (req as { file: { path: string } }).file.path;
-        const originalName = (req as { file: { originalname: string } }).file.originalname;
-                        client
+        const originalName = (req as { file: { originalname: string } }).file
+            .originalname;
+        client
+            .search({
+                index: 'labels',
+                body: {
+                    query: {
+                        constant_score: {
+                            filter: {
+                                term: {
+                                    image_path: originalName,
+                                },
+                            },
+                        },
+                    },
+                },
+            })
+            .then(result => {
+                console.log(
+                    'result',
+                    result.body.hits.hits[0]._source.tfIdf_vector
+                );
+                const tfIdf_vector =
+                    result.body.hits.hits[0]._source.tfIdf_vector;
+                client
                     .search({
                         index: 'labels',
+                        size: 6,
                         body: {
                             query: {
-                                constant_score: {
-                                    filter: {
-                                          term: {
-                                    image_path: originalName
-                                  }
-                                    }
-                                  }
+                                function_score: {
+                                    query: { match_all: {} },
+                                    script_score: {
+                                        script: {
+                                            source:
+                                                "cosineSimilaritySparse (params.query_vector, doc['tfIdf_vector']) + 1.0",
+                                            params: {
+                                                query_vector: tfIdf_vector,
+                                            },
+                                        },
+                                    },
+                                },
                             },
                         },
                     })
                     .then(result => {
-                        console.log('result', result.body.hits.hits[0]._source.tfIdf_vector);
-                        const tfIdf_vector = result.body.hits.hits[0]._source.tfIdf_vector;
-                        client
-                        .search({
-                            index: 'labels',
-                            size: 6,
-                            body: {
-                                query: {
-                                    "function_score": {
-                                        "query": {"match_all" : {}},
-                                            "script_score" : {
-                                                "script" : {
-                                                  "source": "cosineSimilaritySparse (params.query_vector, doc['tfIdf_vector']) + 1.0",
-                                                  "params" : {
-                                                    query_vector : tfIdf_vector
-                                                  } 
-                                                }
-                                            }
-                                          
-                                      }
-                                },
-                            },
-                        }).then(result => {
-                            console.log('result', result.body.hits.hits);
-                       
+                        console.log('result', result.body.hits.hits);
+
                         res.json({
                             elasticSearchResult: result,
-                            imageDescriptions: ["descriptions"],
+                            imageDescriptions: ['descriptions'],
                         });
                         // res.send('' + JSON.stringify(result));
-                         })
-                    })
-                    .catch(reason => console.log('error', reason));
+                    });
+            })
+            .catch(reason => console.log('error', reason));
 
         // classifyImage(imagePath).then(predictions => {
         //     if (predictions && predictions.length > 1) {
