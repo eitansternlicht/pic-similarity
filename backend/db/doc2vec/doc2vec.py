a4ae9d48377686291f6701dbe497e85500726a4c
Eitan Sternlicht <eitanoffun@gmail.com>
2020-07-07 22:51:50 +0300

    re-learn doc2vec, stored learned model in file to be loaded quickly

--- backend/db/doc2vec/doc2vec.py
+++ backend/db/doc2vec/doc2vec.py
@@ -1,7 +1,9 @@
 import os
+import json
+
 from gensim.models.doc2vec import TaggedDocument, Doc2Vec
 from gensim.utils import simple_preprocess
-import json
+from gensim.test.utils import get_tmpfile
 
 DOCS_TOKENS_FILE = 'doc-tokens.txt'
 
@@ -13,7 +15,8 @@ def read_corpus(filename):
 
 
 def train(tagged_docs):
-    model = Doc2Vec(vector_size=300, min_count=1, epochs=200, window = 30, workers=8, dm=0, dbow_words=1)
+    model = Doc2Vec(vector_size=300, min_count=1, epochs=200,
+                    window=30, workers=8, dm=0, dbow_words=1)
     model.build_vocab(tagged_docs)
     model.train(tagged_docs, total_examples=model.corpus_count,
                 epochs=model.epochs)
@@ -21,12 +24,19 @@ def train(tagged_docs):
 
 
 def write_doc_vectors(model, output_filename):
-    with open("doc-vectors.json", "w") as f:
+    with open(output_filename, "w") as f:
         json.dump([list(num.item() for num in model.docvecs[i])
                    for i in range(len(model.docvecs))], f)
 
 
 if __name__ == "__main__":
-    tagged_docs = read_corpus(DOCS_TOKENS_FILE)
-    model = train(tagged_docs)
+    if os.path.exists("model"):
+        model = Doc2Vec.load("model")
+    else:
+        tagged_docs = read_corpus(DOCS_TOKENS_FILE)
+        model = train(tagged_docs)
+    model.delete_temporary_training_data(
+        keep_doctags_vectors=True, keep_inference=True
+    )
+    model.save("model")
     write_doc_vectors(model, "doc-vectors.json")
