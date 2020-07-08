796be550ca948b73b9747e0a631b005e84792745
Eitan Sternlicht <eitanoffun@gmail.com>
2020-07-08 12:02:26 +0300

    minor changes in perf tests

--- backend/performace-tests.ts
+++ backend/performace-tests.ts
@@ -2,10 +2,11 @@ import { average } from './utils/func-utils';
 import { generateRandomImagePath } from './db/image-paths';
 import { queryElastic } from './db/elastic';
 
+const AMOUNT_OF_TESTS_TO_RUN = 100000;
 export const runPerformanceTests = () => {
     console.log('running performance tests');
     const queries = [];
-    for (let i = 0; i < 10000; i++) {
+    for (let i = 0; i < AMOUNT_OF_TESTS_TO_RUN; i++) {
         queries.push(
             queryElastic(generateRandomImagePath()).then(
                 results => ({ doc2vec: results.doc2vec.body.took, tfIdf: results.tfIdf.body.took }),
@@ -17,8 +18,8 @@ export const runPerformanceTests = () => {
         const tfIdfResults = average(results.map(({ tfIdf }) => tfIdf));
         const doc2vecResults = average(results.map(({ doc2vec }) => doc2vec));
 
-        console.log('DONE running performance tests');
-        console.log('tfIdfResults average', tfIdfResults);
-        console.log('doc2vecResults average', doc2vecResults);
+        console.log(`Finsihed running ${AMOUNT_OF_TESTS_TO_RUN} performance tests`);
+        console.log('tfIdf results average query time', tfIdfResults);
+        console.log('doc2vec results average query time', doc2vecResults);
     });
 };
