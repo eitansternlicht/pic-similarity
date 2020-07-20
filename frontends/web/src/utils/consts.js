0ccd31799789685557aec4d1a4304d85938c243d
Eitan Sternlicht <eitanoffun@gmail.com>
2020-07-20 11:12:11 +0300

    update ngrok url

--- frontends/web/src/utils/consts.js
+++ frontends/web/src/utils/consts.js
@@ -1,3 +1,3 @@
 export const SERVER_PORT = 3000;
 // export const SERVER_URL = `http://localhost:${SERVER_PORT}`;
-export const SERVER_URL = `https://14a22ee86c6b.ngrok.io`;
+export const SERVER_URL = `https://807c23203f3e.ngrok.io`;
