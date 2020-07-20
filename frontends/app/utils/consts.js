64cf461c645e703ee5e022f65a3c1f6d356fff96
Eitan Sternlicht <eitanoffun@gmail.com>
2020-07-20 12:28:20 +0300

    fix ngrok url for app

--- frontends/app/utils/consts.js
+++ frontends/app/utils/consts.js
@@ -1,3 +1,3 @@
 export const SERVER_PORT = 3000;
 // export const SERVER_URL = `http://localhost:${SERVER_PORT}`;
-export const SERVER_URL = `https://76027b8aa7d6.ngrok.io`;
+export const SERVER_URL = `https://807c23203f3e.ngrok.io`;
